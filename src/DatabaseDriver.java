df66c43474a4a6bcc3a8a8e1ff48d2c6ac9040f3
Matt Wright <54322905+MatthewWright-Dev@users.noreply.github.com>
2019-12-01 17:14:45 -0800

    Keepin it real

--- src/DatabaseDriver.java
+++ src/DatabaseDriver.java
@@ -4,7 +4,14 @@ public class DatabaseDriver {
     public static void main(String[] args) throws IOException {
 
         //String databasePath = "jdbc:sqlite:C:/users/Matt/Databases/hospTest2.db";
-        String databasePath = "jdbc:sqlite:C:/SQLite/HospitalDB.sl3";
+        /*
+        BuildDB newDB = new BuildDB("C:/users/Matt/Dropbox/Java_Code/Database 2 Project/src/Tables.txt" , "test.sq3");
+        newDB.createNewDatabase(newDB.dbName);
+
+
+         */
+
+        String databasePath = "jdbc:sqlite:C:/SQLite/HospitalDB.db";
         String filePathPerson = "C:/users/Matt/Dropbox/Java_Code/Database 2 Project/src/PersonDataFile1.txt";
         String fileAddDoctor = "C:/users/Matt/Dropbox/Java_Code/Database 2 Project/src/AdditionalDoctor1.txt";
         String fileTreatment = "C:/users/Matt/Dropbox/Java_Code/Database 2 Project/src/TreatmentData.txt";
@@ -14,11 +21,7 @@ public class DatabaseDriver {
         data.doctorData(fileAddDoctor);
         data.treatmentData(fileTreatment);
 
-        //AccessSQL app = new AccessSQL();
 
-        //app.insertDoctor("445434445", "Michael", "Holl", "M", "1995", "12", "1");
-        //app.selectSample();
-        //System.out.println("After Insert");
-        //app.selectSample();
+
     }
 }
